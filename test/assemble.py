4cd5a633c7915f6b08820965cb91aedc163d80d8
Zhijian LIU <zhijianliu.cs@gmail.com>
2016-09-09 16:22:58 +0800

    Modify the script to support Linux.

--- test/assemble.py
+++ test/assemble.py
@@ -1,26 +1,21 @@
 #!/usr/bin/env python
 import os, sys, argparse, platform, binascii
 
-if not platform.system() in ["Darwin", "Linux"]:
-	print "Operator system {} is not supported!".format(platform.system())
+assert platform.system() in ["Darwin", "Linux"]
 
 parser = argparse.ArgumentParser()
 parser.add_argument("-s", dest = "s", default = "assemble.s", type = str)
 parser.add_argument("-o", dest = "o", default = "rom.txt", type = str)
-if platform.system() == "Darwin":
-	parser.add_argument("-bin", dest = "bin", default = "~/mips-gcc-4.8.1/bin", type = str)
-elif platform.system() == "Linux":
-	parser.add_argument("-bin", dest = "bin", default = "~/mips-2014.05/bin", type = str)
 args = parser.parse_args()
 
 if platform.system() == "Darwin":
-	os.system("{}/mips-elf-as -mips32 {} -o rom.o".format(args.bin, args.s))
-	os.system("{}/mips-elf-ld rom.o -o rom.om".format(args.bin))
-	os.system("{}/mips-elf-objcopy -O binary rom.om rom.bin".format(args.bin))
+	args.tc = "~/mips-gcc-4.8.1/bin/mips-elf-"
 elif platform.system() == "Linux":
-	os.system("{}/mips-sde-elf-as -mips32 {} -o rom.o".format(args.bin, args.s))
-	os.system("{}/mips-sde-elf-ld rom.o -o rom.om".format(args.bin))
-	os.system("{}/mips-sde-elf-objcopy -O binary rom.om rom.bin".format(args.bin))
+	args.tc = "~/mips-2014.05/bin/mips-sde-elf-"
+
+os.system("{}as -mips32 {} -o rom.o".format(args.tc, args.s))
+os.system("{}ld -T ram.ld rom.o -o rom.om".format(args.tc))
+os.system("{}objcopy -O binary rom.om rom.bin".format(args.tc))
 
 with open(args.o, "w") as f:
 	s = binascii.b2a_hex(open("rom.bin", "rb").read())
